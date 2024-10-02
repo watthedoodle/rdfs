9196fde8f91a49f3f0804c18b1252747609bb3ff
watthedoodle <watthedoodle@gmail.com>
2024-10-02 16:20:15 +0100

    test out get chunk with base64 encoding, seems to work

--- test-harness/test.js
+++ test-harness/test.js
@@ -13,3 +13,33 @@ Deno.test("auth custom x-rdfs-token works", async () => {
   }).then((x) => x.text().then((data) => ({ status: x.status, body: data })))
     .then((data) => assertEquals(data.status, 200));
 });
+
+Deno.test("can call get-chunk and get 404 on non existent chunk", async () => {
+  let _ = await fetch("http://localhost:8888/get-chunk", {
+    method: "POST",
+    headers: {
+      "x-rdfs-token": "695bfaf2-f381-470b-945c-6cb11fa7a73c",
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({"id": "this-file-does-not-exist"})
+  }).then((x) => x.text().then((data) => ({ status: x.status, body: data })))
+    .then((data) => {
+      console.log(data.body)
+      assertEquals(data.status, 404);
+    });
+});
+
+Deno.test("can call get-chunk and get back data", async () => {
+  let _ = await fetch("http://localhost:8888/get-chunk", {
+    method: "POST",
+    headers: {
+      "x-rdfs-token": "695bfaf2-f381-470b-945c-6cb11fa7a73c",
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({"id": "README.md"})
+  }).then((x) => x.text().then((data) => ({ status: x.status, body: data })))
+    .then((data) => {
+      console.log(data.body)
+      assertEquals(data.status, 200);
+    });
+});
