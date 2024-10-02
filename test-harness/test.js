4c0a0e9269c3a547518fbbf38d2a2ccc53cf6792
watthedoodle <watthedoodle@gmail.com>
2024-10-02 11:36:22 +0100

    added test for the custom x-rdfs-token, auth seems to work as expected

--- test-harness/test.js
+++ test-harness/test.js
@@ -1,6 +1,10 @@
 import { assert, assertEquals } from "jsr:@std/assert";
 
-Deno.test("assert works correctly", () => {
-  assert(true);
-  assertEquals(1, 1);
+Deno.test("auth custom x-rdfs-token works", async () => {
+  let _ = await fetch("http://localhost:8888/", {
+    headers: {
+      "x-rdfs-token": "695bfaf2-f381-470b-945c-6cb11fa7a73c",
+    },
+  }).then((x) => x.text().then((data) => ({ status: x.status, body: data })))
+    .then((data) => assertEquals(data.status, 200));
 });
